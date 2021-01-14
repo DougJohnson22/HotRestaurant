3f114db5a93f25699a8d32eba872139023d82e28
Douglas Johnson <trunks22a@gmail.com>
2021-01-14 14:54:29 -0800

    js update

--- server.js
+++ server.js
@@ -11,38 +11,40 @@ var booked = []
 var waitlist = []
 
 // Home page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"))
 });
 
 // Make a Reservation page
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"))
 });
 
 // View Tables page
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"))
 });
 
 // API Table List link
-app.get("/api/booked", function(req, res) {
+app.get("/api/booked", function (req, res) {
     return res.json(booked)
 });
 
 // API Waitlist
-app.get("/api/waitlist", function(req, res) {
+app.get("/api/waitlist", function (req, res) {
     return res.json(waitlist)
 });
 
 // create a new table/waitlist when user submit a reservation request
-app.post("/api/tables", function(req, res) {
+app.post("/api/booked", function (req, res) {
     var newReservation = req.body
 
     if (booked.length < 5) {
         booked.push(newReservation)
+        return true
     } else {
         waitlist.push(newReservation)
+        return false
     }
 })
 
