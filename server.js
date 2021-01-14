f4522dde0d55f56fb811dd32ad5d16dce8493391
chomieu <chomieu@gmail.com>
2021-01-14 11:31:12 -0800

    Uploaded server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Variables to store customers in queue
+var booked = []
+var waitlist = []
+
+app.get("/", function(req, res) {
+
+});
+
+app.get("/", function(req, res) {
+
+});
+
+app.get("/", function(req, res) {
+
+});
+
+app.get("/", function(req, res) {
+
+});
