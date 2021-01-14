1f952c435bcb935592a45d4cad548b82205d6b4f
Douglas Johnson <trunks22a@gmail.com>
2021-01-14 12:34:59 -0800

    express server setup

--- server.js
+++ server.js
@@ -3,23 +3,51 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Variables to store customers in queue
 var booked = []
 var waitlist = []
 
+// Home page
 app.get("/", function(req, res) {
-
+    res.sendFile(path.join(__dirname, "index.html"))
 });
 
-app.get("/", function(req, res) {
-
+// Make a Reservation page
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"))
 });
 
-app.get("/", function(req, res) {
-
+// View Tables page
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"))
 });
 
-app.get("/", function(req, res) {
+// API Table List link
+app.get("/api/booked", function(req, res) {
+    return res.json(booked)
+});
 
+// API Waitlist
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist)
+});
+
+// create a new table/waitlist when user submit a reservation request
+app.post("/api/tables", function(req, res) {
+    var newReservation = req.body
+
+    if (booked.length < 5) {
+        booked.push(newReservation)
+    } else {
+        waitlist.push(newReservation)
+    }
+})
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
